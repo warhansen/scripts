cbf386ef2fff3054181662fd315d4224927828b9
Warren Hansen <warren.hansen@payd.co>
2018-06-27 10:54:07 +0200

    fix kubernetes script

--- kubernetes_cluster_install/kube-master-only-1.sh
+++ kubernetes_cluster_install/kube-master-only-1.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 yum -y update
-/usr/bin/kubeadm init >> /root/.kubeinit.token
+/usr/bin/kubeadm init >> /root/kubeinit.token
 echo "KUBECONFIG=/etc/kubernetes/admin.conf" >> /etc/environment
+
+echo ""
+echo -e "\033[0;92m After reboot, please check the last paragraph of /root/kubeinit.token file \033[0m"
+echo -e "\033[0;92m for the kubeadm command to run from the worker nodes, in order to join them \033[0m"
+echo -e "\033[0;92m to the cluster \033[0m"
+echo ""
+
+sleep 3
 
 echo "Going for a reboot"
 sleep 3
