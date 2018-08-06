1b7ab6b5ff77afbce907b33d74b3cc6bdbcf52c5
Warren Hansen <warren.hansen@payd.co>
2018-08-06 16:07:07 +0200

    update iptables cloudlfare script

--- iptables_cloudflare_ips_update.sh
+++ iptables_cloudflare_ips_update.sh
@@ -2,12 +2,13 @@
 
 file=/tmp/cloudflare_ips.txt
 interface=enp0s3
+port=443
 
 curl -k -o $file https://www.cloudflare.com/ips-v4
 
 while read line; do
-   iptables -D INPUT -i $interface -s $line -j ACCEPT
-   iptables -A INPUT -i $interface -s $line -j ACCEPT
+   iptables -D INPUT -i $interface -s $line --dport $port -j ACCEPT
+   iptables -A INPUT -i $interface -s $line --dport $port -j ACCEPT
 done <$file
 
 iptables-save > /etc/sysconfig/iptables
