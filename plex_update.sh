1fa3c3e58fc7784b97a7f76ebf70b0c872cef3cc
Warren Hansen <warhansen@gmail.com>
2019-07-19 15:38:08 +0200

    updated plex update

--- plex_update.sh
+++ plex_update.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-file=/root/update_file.plex
-download_url=$(cat $file)
+rpm -q jq
+if [ $? -ne 0 ]; then
+    yum -y install jq
+else
+    echo "jq installed"
+fi
 
-if [ -e $file ]; then
+
+plex_download_url=https://plex.tv/api/downloads/5.json
+plex_download_file=/root/.5.json
+old_plex_download_file=/root/.old_5.json
+
+wget $plex_download_url -O $plex_download_file
+download_url=$(jq .computer $plex_download_file | grep "redhat/plexmediaserver" | grep "x86_64" | sed 's/"url": "//g' | sed 's/",//g' | tr -d [:space:])
+
+diff  $plex_download_file  $old_plex_download_file
+
+if [ $? -ne 0 ]; then
     wget $download_url -O /root/plex_update.rpm
     yum -y install /root/plex_update.rpm
     rm -rf /root/plex_update
@@ -11,4 +25,8 @@ if [ -e $file ]; then
     systemctl stop plexmediaserver
     sleep 15
     systemctl start plexmediaserver
+else
+    echo "No updates available"
 fi
+
+mv $plex_download_file $old_plex_download_file
