39b1b48bfd1bbff8f90045393922d0ff71e90fd3
Warren Hansen <warhansen@gmail.com>
2019-07-19 15:43:28 +0200

    updated plex update

--- centos_plex_update.sh
+++ centos_plex_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Install requirements
 rpm -q jq
 if [ $? -ne 0 ]; then
     yum -y install jq
@@ -7,16 +8,24 @@ else
     echo "jq installed"
 fi
 
-
+## ensure no failures if there are no old versions
 plex_download_url=https://plex.tv/api/downloads/5.json
 plex_download_file=/root/.5.json
 old_plex_download_file=/root/.old_5.json
 
+if [ -e $old_plex_download_file ]; then
+    echo "there is an old version"
+else
+    touch $old_plex_download_file
+fi
+
+## Cleaning up the json file
 wget $plex_download_url -O $plex_download_file
 download_url=$(jq .computer $plex_download_file | grep "redhat/plexmediaserver" | grep "x86_64" | sed 's/"url": "//g' | sed 's/",//g' | tr -d [:space:])
 
 diff  $plex_download_file  $old_plex_download_file
 
+## Upgrading if necessary
 if [ $? -ne 0 ]; then
     wget $download_url -O /root/plex_update.rpm
     yum -y install /root/plex_update.rpm
@@ -29,4 +38,5 @@ else
     echo "No updates available"
 fi
 
+## Recording old version
 mv $plex_download_file $old_plex_download_file
