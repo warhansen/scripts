64cfb4c237ce785e4e9888ccc1e0f2fdbbbdef6a
Warren Hansen <warhansen@gmail.com>
2019-11-28 11:34:00 +0200

    updated emby_update

--- centos_emby_update.sh
+++ centos_emby_update.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+## Download git page to check version later
 curl -o emby_check_update https://github.com/MediaBrowser/Emby.Releases/releases/
 
+## Extract version from above page
 grep "/MediaBrowser/Emby.Releases/tree" emby_check_update > new_emby_version
 version=$(cat new_emby_version | sed 1q  | egrep -o '[0-9].*' | egrep -o '^[^"]*"' | sed 's/"//g')
 echo "Current released version is: " $version
 echo $version > emby_released_version
 
+## If this is the first time this script is run, do nothing, else match versions, if newer version is out, do upgrade
 if [ -f previous_emby_released_version ];then
     if diff emby_released_version previous_emby_released_version; then
         echo "There are no updates"
@@ -18,4 +23,5 @@ if [ -f previous_emby_released_version ];then
     fi
 fi
 
+## Record current version for future use
 mv emby_released_version previous_emby_released_version
