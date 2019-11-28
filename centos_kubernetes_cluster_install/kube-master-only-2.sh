f3edecec58ca0685aa6d2b2eee797ac7dd80a60a
Warren Hansen <warhansen@gmail.com>
2019-11-28 14:33:19 +0200

    updated

--- centos_kubernetes_cluster_install/kube-master-only-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export kubever=$(kubectl version | base64 | tr -d '\n') && kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-
-echo "Going for a reboot"
-sleep 3
-reboot
